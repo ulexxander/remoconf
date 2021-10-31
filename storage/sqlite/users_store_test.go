7bd245dc3e339d9a1026c4892d9c324a65fbd56a
Alexander Ustyugov <ulexxander@gmail.com>
2021-10-31 21:15:07 +0100

    moved setup db code from users_store_test.go to testutil/sqlite.go

--- storage/sqlite/users_store_test.go
+++ storage/sqlite/users_store_test.go
@@ -1,19 +1,17 @@
 package sqlite_test
 
 import (
-	"path/filepath"
 	"testing"
 
-	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/ulexxander/remoconf/storage"
 	"gitlab.com/ulexxander/remoconf/storage/sqlite"
+	"gitlab.com/ulexxander/remoconf/testutil"
 )
 
 func TestUsersStore(t *testing.T) {
-	db := setupDBTest(t)
+	db := testutil.SetupDBTest(t)
 	us := sqlite.NewUsersStore(db)
 
 	t.Run("user does not exist", func(t *testing.T) {
@@ -38,32 +36,4 @@ func TestUsersStore(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, u)
 	})
-}
-
-func setupDBTest(t *testing.T) *sqlx.DB {
-	db, err := setupDB(t.TempDir())
-	if err != nil {
-		t.Fatalf("failed to setup db: %s", err)
-	}
-	t.Cleanup(func() {
-		if err := db.Close(); err != nil {
-			t.Logf("error closing db: %s", err)
-		}
-	})
-	return db
-}
-
-func setupDB(dir string) (*sqlx.DB, error) {
-	path := filepath.Join(dir, "sqlite.db")
-	db, err := sqlx.Open("sqlite3", path)
-	if err != nil {
-		return nil, errors.Wrap(err, "opening")
-	}
-	if err := db.Ping(); err != nil {
-		return nil, errors.Wrap(err, "pinging")
-	}
-	if err := sqlite.Migrate(db); err != nil {
-		return nil, errors.Wrap(err, "migrating")
-	}
-	return db, nil
 }
---
+++ testutil/sqlite.go
@@ -0,0 +1,38 @@
+package testutil
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"gitlab.com/ulexxander/remoconf/storage/sqlite"
+)
+
+func SetupDBTest(t *testing.T) *sqlx.DB {
+	db, err := SetupDB(t.TempDir())
+	if err != nil {
+		t.Fatalf("failed to setup db: %s", err)
+	}
+	t.Cleanup(func() {
+		if err := db.Close(); err != nil {
+			t.Logf("error closing db: %s", err)
+		}
+	})
+	return db
+}
+
+func SetupDB(dir string) (*sqlx.DB, error) {
+	path := filepath.Join(dir, "sqlite.db")
+	db, err := sqlx.Open("sqlite3", path)
+	if err != nil {
+		return nil, errors.Wrap(err, "opening")
+	}
+	if err := db.Ping(); err != nil {
+		return nil, errors.Wrap(err, "pinging")
+	}
+	if err := sqlite.Migrate(db); err != nil {
+		return nil, errors.Wrap(err, "migrating")
+	}
+	return db, nil
+}
