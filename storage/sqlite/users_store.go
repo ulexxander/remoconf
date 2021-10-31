6995be9e07c518e92a7cbf8cb67eb7b3b989b640
Alexander Ustyugov <ulexxander@gmail.com>
2021-10-31 20:58:33 +0100

    UsersStore - table, implemented GetByID, Create methods

--- storage/sqlite/users_store.go
+++ storage/sqlite/users_store.go
@@ -1,14 +1,72 @@
 package sqlite
 
-import "gitlab.com/ulexxander/remoconf/storage"
+import (
+	"database/sql"
+	"fmt"
+
+	"gitlab.com/ulexxander/remoconf/storage"
+)
 
 type UsersStore struct {
+	db *sql.DB
 }
+
+func NewUsersStore(db *sql.DB) *UsersStore {
+	return &UsersStore{db}
+}
+
+const migrationUp = `
+CREATE TABLE users (
+	id integer PRIMARY KEY AUTOINCREMENT,
+  login text NOT NULL,
+  password text NOT NULL,
+	created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at timestamp
+)
+`
+
+const migrationDown = `
+DROP TABLE IF EXISTS users
+`
+
+func (us *UsersStore) Migrate() error {
+	_, err := us.db.Exec(migrationDown)
+	if err != nil {
+		return fmt.Errorf("migration down: %w", err)
+	}
+	_, err = us.db.Exec(migrationUp)
+	if err != nil {
+		return fmt.Errorf("migration up: %w", err)
+	}
+	return nil
+}
+
+const getByIDQuery = `SELECT * FROM users WHERE id = $1`
 
 func (us *UsersStore) GetByID(id int) (*storage.User, error) {
-	return nil, nil
+	row := us.db.QueryRow(getByIDQuery, id)
+	var u storage.User
+	if err := row.Scan(
+		&u.ID,
+		&u.Login,
+		&u.Password,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+	); err != nil {
+		return nil, err
+	}
+	return &u, nil
 }
 
-func (us *UsersStore) Create(p storage.UserCreateParams) (*storage.User, error) {
-	return nil, nil
+const createQuery = `INSERT INTO users (login, password)
+VALUES ($1, $2)
+RETURNING id`
+
+func (us *UsersStore) Create(p storage.UserCreateParams) (int, error) {
+	row := us.db.QueryRow(createQuery, p.Login, p.Password)
+	var id int
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
 }
--- storage/sqlite/users_store_test.go
+++ storage/sqlite/users_store_test.go
@@ -1,13 +1,22 @@
 package sqlite_test
 
 import (
+	"database/sql"
+	"fmt"
+	"path/filepath"
 	"testing"
 
+	_ "github.com/mattn/go-sqlite3"
+	"gitlab.com/ulexxander/remoconf/storage"
 	"gitlab.com/ulexxander/remoconf/storage/sqlite"
 )
 
 func TestUsersStore(t *testing.T) {
-	us := sqlite.UsersStore{}
+	db := setupDBTest(t)
+	us := sqlite.NewUsersStore(db)
+	if err := us.Migrate(); err != nil {
+		t.Fatalf("failed migrating: %s", err)
+	}
 
 	t.Run("user does not exist", func(t *testing.T) {
 		u, err := us.GetByID(1)
@@ -18,4 +27,47 @@ func TestUsersStore(t *testing.T) {
 			t.Fatalf("expected user to be nil, got %v", u)
 		}
 	})
+
+	var createdUserID int
+
+	t.Run("creating user", func(t *testing.T) {
+		id, err := us.Create(storage.UserCreateParams{
+			Login:    "alex",
+			Password: "123",
+		})
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		createdUserID = id
+	})
+
+	t.Run("getting created user", func(t *testing.T) {
+		u, err := us.GetByID(createdUserID)
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+		if u == nil {
+			t.Fatalf("user must not be nil")
+		}
+	})
+}
+
+func setupDBTest(t *testing.T) *sql.DB {
+	db, err := setupDB(t.TempDir())
+	if err != nil {
+		t.Fatalf("failed to setup db: %s", err)
+	}
+	return db
+}
+
+func setupDB(dir string) (*sql.DB, error) {
+	path := filepath.Join(dir, "sqlite.db")
+	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, fmt.Errorf("opening: %w", err)
+	}
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("pinging: %w", err)
+	}
+	return db, nil
 }
