14ebfadf0fd4dab671a4adc60a71127c28e7fa57
Alexander Ustyugov <ulexxander@gmail.com>
2021-10-31 20:27:50 +0100

    package storage and added few models - User, Project, Config

---
+++ storage/models.go
@@ -0,0 +1,30 @@
+package storage
+
+import "time"
+
+type User struct {
+	ID        int
+	Login     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt *time.Time
+}
+
+type Project struct {
+	ID          int
+	Title       string
+	Description string
+	CreatedAt   time.Time
+	CreatedBy   int
+	UpdatedAt   *time.Time
+	UpdatedBy   *int
+}
+
+type Config struct {
+	ID        int
+	ProjectID uint
+	Version   int
+	Content   string
+	CreatedAt time.Time
+	CreatedBy int
+}
