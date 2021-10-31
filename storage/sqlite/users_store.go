704fd9436e6c6ac12fec6de7d0abda3f0bdf292c
Alexander Ustyugov <ulexxander@gmail.com>
2021-10-31 21:23:25 +0100

    users_store - renamed query constants to be more specific

--- storage/sqlite/users_store.go
+++ storage/sqlite/users_store.go
@@ -13,23 +13,23 @@ func NewUsersStore(db *sqlx.DB) *UsersStore {
 	return &UsersStore{db}
 }
 
-const getByIDQuery = `SELECT * FROM users WHERE id = $1`
+const userGetByIDQuery = `SELECT * FROM users WHERE id = $1`
 
 func (us *UsersStore) GetByID(id int) (*storage.User, error) {
 	var u storage.User
-	if err := us.db.Get(&u, getByIDQuery, id); err != nil {
+	if err := us.db.Get(&u, userGetByIDQuery, id); err != nil {
 		return nil, err
 	}
 	return &u, nil
 }
 
-const createQuery = `INSERT INTO users (login, password)
+const userCreateQuery = `INSERT INTO users (login, password)
 VALUES ($1, $2)
 RETURNING id`
 
 func (us *UsersStore) Create(p storage.UserCreateParams) (int, error) {
 	var id int
-	if err := us.db.Get(&id, createQuery, p.Login, p.Password); err != nil {
+	if err := us.db.Get(&id, userCreateQuery, p.Login, p.Password); err != nil {
 		return 0, err
 	}
 	return id, nil
